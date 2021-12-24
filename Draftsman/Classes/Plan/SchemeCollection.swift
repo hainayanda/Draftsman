7adf1e1c63eab49b3945549634281f2028ade421
Nayanda Haberty <hainayanda@outlook.com>
2021-12-24 17:02:39 +0800

    refactor scheme collection build view to inteligently removed unused view if in view plan

--- Draftsman/Classes/Plan/SchemeCollection.swift
+++ Draftsman/Classes/Plan/SchemeCollection.swift
@@ -10,25 +10,37 @@ import Foundation
 import UIKit
 
 open class SchemeCollection: ViewPlan {
-    public var context: PlanContext = PlanContext(currentView: UIView())
+    public var context: PlanContext
     public var subPlan: [ViewScheme]
     
-    public init(subPlan: [ViewScheme]) {
+    public init(subPlan: [ViewScheme], inViewPlan: Bool = false) {
+        self.context = PlanContext(currentView: UIView(), inViewPlan: inViewPlan)
         self.subPlan = subPlan
     }
     
     open func build(for view: UIView) -> [NSLayoutConstraint] {
         context.currentView = view
         var constraints: [NSLayoutConstraint] = []
+        if context.inViewPlan {
+            view.subviews.forEach { subview in
+                if !subPlan.contains(where: { $0.view == subview }) {
+                    subview.removeFromSuperview()
+                }
+            }
+        }
         subPlan.forEach { scheme in
             scheme.context = context
             let viewScheme = scheme.view
             viewScheme.translatesAutoresizingMaskIntoConstraints = false
             if let stack = view as? UIStackView, scheme.isStackContent {
+                if stack.arrangedSubviews.contains(viewScheme) {
+                    viewScheme.removeFromSuperview()
+                }
                 stack.addArrangedSubview(viewScheme)
             } else {
                 view.addSubview(viewScheme)
             }
+            view.bringSubviewToFront(viewScheme)
             if let controller = view.nextViewController,
                let schemeController = viewScheme.nextViewController,
                controller != schemeController {
