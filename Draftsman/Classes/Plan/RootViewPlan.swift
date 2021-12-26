604b7d1506829d14248397ac014fd7627f215cd3
Nayanda Haberty <hainayanda@outlook.com>
2021-12-26 17:12:02 +0800

    Refactor RootViewPlan

--- Draftsman/Classes/Plan/RootViewPlan.swift
+++ Draftsman/Classes/Plan/RootViewPlan.swift
@@ -9,13 +9,68 @@ import Foundation
 #if canImport(UIKit)
 import UIKit
 
-final class RootViewPlan: SchemeCollection {
-    public weak var delegate: PlanDelegate?
+open class RootViewPlan: SchemeCollection {
+    weak var delegate: PlanDelegate?
     
-    override func build(for view: UIView) -> [NSLayoutConstraint] {
+    open override func build(for view: UIView) -> [NSLayoutConstraint] {
+        buildAndExtractConstraint(for: view).toActivated
+    }
+    
+    @discardableResult
+    open override func apply(for view: UIView) -> [NSLayoutConstraint] {
+        let constraints = buildAndExtractConstraint(for: view)
+        NSLayoutConstraint.deactivate(constraints.toRemoved)
+        NSLayoutConstraint.activate(constraints.toActivated)
+        return constraints.toActivated
+    }
+    
+    func buildAndExtractConstraint(for view: UIView) -> ExtractedConstraints {
+        buildWithContext(for: view) {
+            let constraints: [NSLayoutConstraint] = subPlan.reduce([]) { partialResult, scheme in
+                var nextConstraints = buildScheme(scheme, forView: view)
+                nextConstraints.append(contentsOf: partialResult)
+                return nextConstraints
+            }
+            return extractConstraints(for: view, from: constraints)
+        }
+    }
+    
+    func buildWithContext(for view: UIView, _ builder: () -> ExtractedConstraints) -> ExtractedConstraints {
         context.currentView = view
-        context._delegate = delegate
-        return super.build(for: view)
+        if context.inViewPlan {
+            removeSubviewThatNotInPlan(for: view)
+        }
+        return builder()
+    }
+    
+    func extractAllViewPlanIds() -> [String] {
+        subPlan.reduce([]) { partialResult, scheme in
+            var nextResult = partialResult
+            nextResult.append(contentsOf: scheme.managedViewPlanIds)
+            return nextResult
+        }.unique
+    }
+    
+    func extractConstraints(for view: UIView, from constraints: [NSLayoutConstraint]) -> ExtractedConstraints {
+        let currentConstraints = view.mostTopView.allConstraints
+        let combinedConstraints: [NSLayoutConstraint] = constraints.compactMap { constraint in
+            guard let found = currentConstraints.first(where: { $0 ~= constraint }) else {
+                return constraint
+            }
+            found.resembling(constraint)
+            return found
+        }
+        if context.inViewPlan {
+            let viewPlanIds = extractAllViewPlanIds()
+            let removedConstraints = currentConstraints.filter { constraint in
+                for viewPlanId in viewPlanIds where constraint.isPartOf(viewPlanId: viewPlanId) {
+                    return true
+                }
+                return false
+            }
+            return .init(toActivated: combinedConstraints, toRemoved: removedConstraints)
+        }
+        return .init(toActivated: combinedConstraints, toRemoved: [])
     }
 }
 #endif
