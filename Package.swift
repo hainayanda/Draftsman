fa511c05c34df5351179270f76e0b3b4544ef8a8
Nayanda Haberty <hainayanda@outlook.com>
2021-12-29 14:45:50 +0800

    Fix swift package manager build

--- Package.swift
+++ Package.swift
@@ -15,14 +15,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/hainayanda/Clavier.git", from: "1.0.2"),
+        .package(url: "https://github.com/hainayanda/Clavier.git", from: "1.0.3"),
+        .package(url: "https://github.com/hainayanda/Builder.git", from: "1.0.3"),
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0")
     ],
     targets: [
         .target(
             name: "Draftsman",
-            dependencies: ["Clavier"],
+            dependencies: ["Clavier", "Builder"],
             path: "Draftsman/Classes"
         ),
         .testTarget(
