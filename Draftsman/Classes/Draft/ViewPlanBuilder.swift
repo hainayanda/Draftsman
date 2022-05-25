3500fdea628c5fa9fc22c9058173eb2ff0621165
Nayanda Haberty <hainayanda@outlook.com>
2022-05-25 10:34:32 +0700

    Move LayoutDraft basic building to ViewPlanBuilder

--- Draftsman/Classes/Draft/ViewPlanBuilder.swift
+++ Draftsman/Classes/Draft/ViewPlanBuilder.swift
@@ -11,6 +11,7 @@ import UIKit
 
 open class ViewPlanBuilder: ViewPlan {
     var plans: [ViewDraft]
+    var allPlans: [ViewDraft] { plans }
     open var insertablePlans: [ViewDraft] { plans }
     var constraintBuilders: [ConstraintBuilder] = []
     public var context: PlanContext?
@@ -20,21 +21,9 @@ open class ViewPlanBuilder: ViewPlan {
     }
     
     open func build(for view: UIView) -> [NSLayoutConstraint] {
-        let context = context ?? PlanContext(view: view)
-        context.currentView = view
-        let viewController = view.responderViewController
-        return insertablePlans.reduce(constraintBuilders.build(using: context)) { partialResults, plan in
-            if let root = context.root {
-                plan.view.makeAssociated(with: root)
-            }
-            view.addSubview(plan.view)
-            if let currentViewController = viewController,
-               let subViewController = plan.view.responderViewController,
-                subViewController != currentViewController {
-                currentViewController.addChild(subViewController)
-            }
-            plan.context = context
-            return partialResults.added(withContentsOf: plan.build())
+        prepareBuild(for: view)
+        return buildSubview(plans: plans, for: getContext(for: view)) {
+            view.addSubview($0)
         }.validUniques
     }
     
@@ -43,6 +32,68 @@ open class ViewPlanBuilder: ViewPlan {
         let constraints = build(for: view)
         NSLayoutConstraint.activate(constraints)
         return constraints
+    }
+    
+    func getContext(for view: UIView) -> PlanContext {
+        let context = context ?? PlanContext(view: view)
+        context.currentView = view
+        return context
+    }
+    
+    func removeAssociatedSubview(in view: UIView) {
+        guard let root = context?.root else { return }
+        view.subviews.forEach { subview in
+            guard subview.isPartOf(planned: root),
+                  !allPlans.contains(where: { $0.view == subview }) else { return }
+            subview.removeFromSuperview()
+            if let currentViewController = view.responderViewController,
+                let subViewController = subview.responderViewController,
+                subViewController != currentViewController {
+                subViewController.removeFromParent()
+            }
+        }
+    }
+    
+    func buildSubview(plans: [ViewDraft], for context: PlanContext, doAdd: (UIView) -> Void) -> [NSLayoutConstraint] {
+        let viewController = context.currentViewController
+        return plans.reduce([]) { partialResults, plan in
+            if let root = context.root {
+                plan.view.makeAssociated(with: root)
+            }
+            doAdd(plan.view)
+            if let currentViewController = viewController,
+               let subViewController = plan.view.responderViewController,
+                subViewController != currentViewController {
+                currentViewController.addChild(subViewController)
+            }
+            plan.context = context
+            return partialResults.added(withContentsOf: plan.build())
+        }
+    }
+    
+    func prepareBuild(for view: UIView) {
+        removeAssociatedSubview(in: view)
+        if shouldNotTranslatesAutoresizingMaskIntoConstraints(for: view) {
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    func shouldNotTranslatesAutoresizingMaskIntoConstraints(for view: UIView) -> Bool {
+        if view is UITableViewCell || view is UICollectionViewCell {
+            return false
+        }
+        guard let responder = view.next else {
+            return false
+        }
+        if responder is UIViewController {
+            return false
+        } else if let cell = responder as? UITableViewCell, cell.contentView == view {
+            return false
+        } else if let cell = responder as? UICollectionViewCell, cell.contentView == view {
+            return false
+        } else {
+            return true
+        }
     }
 }
 #endif
